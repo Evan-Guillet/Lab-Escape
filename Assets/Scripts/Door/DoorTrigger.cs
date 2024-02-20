5854c361122e365804e8c72ce94997bc4d09d192
fmaxime <maxime.fuzeau@ynov.com>
2024-02-20 13:16:27 +0100

    :sparkles: | make chest prefab with trigger

--- Assets/Scripts/Door/DoorTrigger.cs
+++ Assets/Scripts/Door/DoorTrigger.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
-    public Animator doorAnimator; // Référence à l'Animator de la porte
-    public string openTrigger = "Player"; // Nom du déclencheur d'ouverture dans l'Animator
-
-    // Déclenché lorsque le Collider2D entre en collision avec un autre Collider2D
+    public Animator doorAnimator;
+    public string openTrigger = "Player";
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") )// and player.key == true)
         {
-            // Si le joueur entre dans la zone de détection, déclencher l'animation d'ouverture de la porte
             doorAnimator.SetTrigger(openTrigger);
         } else if (collision.CompareTag("Enemy"))
         {
@@ -18,15 +15,13 @@ public class DoorTrigger : MonoBehaviour
         } else {
             doorAnimator.SetTrigger("Close");
         }
-    }
-
-    // Déclenché lorsque le Collider2D perd la collision avec un autre Collider2D
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
         {
-            // Si le joueur sort de la zone de détection, déclencher l'animation de fermeture de la porte
             doorAnimator.SetTrigger(openTrigger);
+        } else if (collision.CompareTag("Enemy"))
+        {
+            doorAnimator.SetTrigger(openTrigger);
+        } else {
+            doorAnimator.SetTrigger("Close");
         }
     }
 }
