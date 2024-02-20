ed55fd1c5c599fc6f70459f751d0dae1922c68af
Harcadakad <evan.guillet@ynov.com>
2024-02-20 16:24:15 +0100

    :hammer: Adds boss.cs

--- Assets/Scripts/Enemies/Boss.cs
+++ Assets/Scripts/Enemies/Boss.cs
@@ -1,32 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Panda;
 
 public class Boss : MonoBehaviour {
+
+    NavMeshAgent agent;
 
     SpriteRenderer spriteRenderer;
     Animator animator;
     public GameObject PerceivedTarget = null;
     
     void Start(){
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
 
     void Update(){
-        if(Input.GetButtonDown("F1")){
+        /*
+        if(Input.GetKeyDown(KeyCode.F1)){
             animator.SetTrigger("Idle");
 
-        } else if(Input.GetButtonDown("F2")){
-            animator.SetTrigger("Run");
+        } else if(Input.GetKeyDown(KeyCode.F2)){
+            animator.SetBool("Run", true);
 
-        } else if(Input.GetButtonDown("F3")){
-            animator.SetTrigger("Idle_2");
-
-        } else if(Input.GetButtonDown("F4")){
-            animator.SetTrigger("Run_2");
+        } else if(Input.GetKeyDown(KeyCode.F3)){
+            animator.SetTrigger("Death");
 
         }
-        
+        */
+        FlipSpriteToX();
+    }
+
+    void FlipSpriteToX(){
+        if(agent.velocity.x < 0){
+            spriteRenderer.flipX = true;
+
+        } else if(agent.velocity.x > 0){
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    [Task]
+    void PursueTarget(){
+        if(PerceivedTarget == null){
+            Task.current.Fail();
+            return;
+        }
+        animator.SetBool("IsRunning", agent.velocity.x != 0 || agent.velocity.y != 0);
+        agent.SetDestination(PerceivedTarget.transform.position);
+        if(agent.remainingDistance <= 4){
+            Task.current.Fail();
+            return;
+        }
     }
 }
