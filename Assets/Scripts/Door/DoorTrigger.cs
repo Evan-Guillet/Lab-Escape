1e2c6c93a23f928dee75ed72fb391c1b19890562
fmaxime <maxime.fuzeau@ynov.com>
2024-02-20 13:43:15 +0100

    :twisted_rightwards_arrows: | merge map

--- Assets/Scripts/Door/DoorTrigger.cs
+++ Assets/Scripts/Door/DoorTrigger.cs
@@ -6,15 +6,7 @@ public class DoorTrigger : MonoBehaviour
     public string openTrigger = "Player";
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") )// and player.key == true)
-        {
-            doorAnimator.SetTrigger(openTrigger);
-        } else if (collision.CompareTag("Enemy"))
-        {
-            doorAnimator.SetTrigger(openTrigger);
-        } else {
-            doorAnimator.SetTrigger("Close");
-        }
+        if (collision.CompareTag("Player"))// and player.key == true)
         {
             doorAnimator.SetTrigger(openTrigger);
         } else if (collision.CompareTag("Enemy"))
