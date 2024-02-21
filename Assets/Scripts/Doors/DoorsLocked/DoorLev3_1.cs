7aa4585ced2a78a9fd85f037889a7f4038eb66f5
fmaxime <maxime.fuzeau@ynov.com>
2024-02-21 13:33:55 +0100

    :sparkles: | make system of key -> chest -> door -> open

---
+++ Assets/Scripts/Doors/DoorsLocked/DoorLev3_1.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class DoorLev3_1 : MonoBehaviour
+{
+    public Animator doorAnimator;
+    public BoxCollider2D collider;
+    public string openTrigger = "Player";
+    public Player player;
+    public bool key = false;
+
+    private void Start()
+    {
+        if (player != null)
+        {
+            if (player.doorLev3_1)
+            {
+                key = true;
+            }
+        }
+        else
+        {
+            Debug.LogError("Player GameObject non assigné à DoorLev3_1.cs");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && key)
+        {
+            collider.isTrigger = true;
+            doorAnimator.SetTrigger(openTrigger);
+        }
+    }
+}
