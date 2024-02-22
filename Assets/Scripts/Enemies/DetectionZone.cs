51c2e39b8ebf835be108315058e03e816d774e25
Evan-Guillet <guillet_evan@protonmail.com>
2024-02-22 09:38:33 +0100

    :hammer: Adds a condition for detection zone trigger, to find out whether it's a boss or a kamikaze who detects the player

--- Assets/Scripts/Enemies/DetectionZone.cs
+++ Assets/Scripts/Enemies/DetectionZone.cs
@@ -6,13 +6,21 @@ public class DetectionZone : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.tag == "Player"){
-            transform.parent.GetComponent<Boss>().PerceivedTarget = collider.gameObject;
+            if(transform.parent.tag == "Boss")
+                transform.parent.GetComponent<Boss>().PerceivedTarget = collider.gameObject;
+
+            if(transform.parent.tag == "Kamikaze")
+                transform.parent.GetComponent<Kamikaze>().PerceivedTarget = collider.gameObject;
         }
     }
 
     void OnTriggerExit2D(Collider2D collider){
         if(collider.tag == "Player"){
-            transform.parent.GetComponent<Boss>().PerceivedTarget = null;
+            if(transform.parent.tag == "Boss")
+                transform.parent.GetComponent<Boss>().PerceivedTarget = null;
+
+            if(transform.parent.tag == "Kamikaze")
+                transform.parent.GetComponent<Kamikaze>().PerceivedTarget = null;
         }
     }
 }
